d8b71aef2165dcc686316b173e22d5f314222d21
ersaiurs <ersaliasyh@gmail.com>
2025-07-19 17:27:00 +0700

    fix: enable static export in next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ✅ Matikan linting saat build di Vercel
   },
+  output: 'export', // ✅ Tambahkan ini agar semua halaman statis diekspor
 };
 
 module.exports = nextConfig;
